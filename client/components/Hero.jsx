77563c8d9c29bd4fda19a166aff87f526ae10c37
arunakamara <kamaraaruna198@gmail.com>
2025-08-16 11:29:48 +0530

    name changed

--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -15,13 +15,13 @@ const Hero = () => {
   return (
     <section className="py-20 text-center">
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl md:text-5xl font-bold text-green-800 mb-4 bg-amber-50 rounded-full shadow-2xl shadow-amber-100">
+        <h1 className="text-4xl md:text-5xl font-bold text-white mb-4 bg-green-500 rounded-full shadow-2xl shadow-amber-100">
           Welcome to SLSU-AP
         </h1>
         <p className="bg-white md:text-xl text-gray-150 font-bold text-2xl mb-6 mt-10 rounded-2xl">
           Uniting Sierra Leonean students in Andhra Pradesh with pride and purpose.
         </p>
-        <p className="text-base md:text-lg text-blue-600 mb-8 bg-amber-50 rounded-2xl">
+        <p className="text-base md:text-lg text-white mb-8 bg-blue-500 rounded-2xl">
           <strong>Mission:</strong> Building a community that celebrates our heritage and supports academic excellence.
         </p>
         <button
