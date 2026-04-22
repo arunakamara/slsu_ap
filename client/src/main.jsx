82cfb11123d217140ed51f362afa3bd9d4a1e4e5
arunakamara <kamaraaruna198@gmail.com>
2026-04-23 01:06:37 +0530

    2.4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,17 @@
 import * as Sentry from "@sentry/react";
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
 Sentry.init({
-  dsn: "https://e15d2611ae581425f93aee5683dc6201@o4508959386697728.ingest.us.sentry.io/4508959391875072"
+  dsn: "https://e15d2611ae581425f93aee5683dc6201@o4508959386697728.ingest.us.sentry.io/4508959391875072",
 });
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
