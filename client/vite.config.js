6b39344057afa82b7c95a7c5aadfe41286b93477
arunakamara <kamaraaruna198@gmail.com>
2026-04-23 11:12:54 +0530

    2.8

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { copyFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    {
-      name: "copy-redirects",
-      closeBundle() {
-        copyFileSync("_redirects", "dist/_redirects");
-      },
-    },
-  ],
-  build: {
-    outDir: "dist",
-  },
+  plugins: [react(), tailwindcss()],
 });
